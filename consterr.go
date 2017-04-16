2a18be34ed167ea636c95d2c9a631f1502e12057
Jaden Weiss <jadr2ddude@gmail.com>
2017-04-16 13:08:29 -0400

    fix typo

--- consterr.go
+++ consterr.go
@@ -1,9 +1,10 @@
-//Package consterr is another way of implementing
+//Package consterr is another way of implementing constant errors in Go
 package consterr
 
 //Error is another way to create errors
 type Error string
 
+//Error implements the error interface
 func (e Error) Error() string {
 	return string(e)
 }
